8a5503802695acdd1a20544bf5cf5c67576bb25d
realChakrawarti <anupam.chakrawarti@outlook.com>
2025-07-27 01:39:16 +0530

    feat: add filtering subreddit, update subreddit width

--- src/shared/ui/sonner.tsx
+++ src/shared/ui/sonner.tsx
@@ -18,7 +18,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
-          description: "group-[.toast]:text-muted-foreground",
+          description:
+            "group-[.toast]:text-muted-foreground text-muted-foreground",
           title: "text-sm",
           toast:
             "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
--- src/views/public-catalog/subreddit-posts.tsx
+++ src/views/public-catalog/subreddit-posts.tsx
@@ -2,6 +2,7 @@ import type { ZCatalogSubredditPost } from "~/entities/catalogs/models";
 
 import { PublicMarker } from "~/widgets/public-layout";
 
+import { FilterSubreddit } from "./filter-subreddit";
 import PostCard from "./post-card";
 
 export default function SubredditPosts({
@@ -9,6 +10,8 @@ export default function SubredditPosts({
 }: {
   posts: ZCatalogSubredditPost[] | undefined;
 }) {
+  const subreddits = new Set(posts?.map((post) => post.subreddit));
+
   if (posts?.length) {
     return (
       <section className="space-y-4">
@@ -17,6 +20,7 @@ export default function SubredditPosts({
           <h2 id={"subreddit-posts"} className="text-lg">
             <a href={"#subreddit-posts"}>Subreddit Posts</a>
           </h2>
+          <FilterSubreddit subreddits={Array.from(subreddits)} />
         </div>
 
         <PostCard posts={posts} />
