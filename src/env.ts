40d1bf0ba41d9c843cacbbb6c497fca6ab277da2
realChakrawarti <anupam.chakrawarti@outlook.com>
2026-06-04 18:43:59 +0530

    fix: `global.d.ts` ambient context

---
+++ src/env.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+export const envVariables = z.object({
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  CLOUDFLARE_API_TOKEN: z.string(),
+  CLOUDFLARE_KV_NAMESPACE_ID: z.string(),
+  ENABLE_CATALOG_UPDATE: z
+    .enum(["true", "false"])
+    .transform((v) => v === "true"),
+  FIREBASE_CLIENT_EMAIL: z.string(),
+  FIREBASE_PRIVATE_KEY: z.string(),
+  GOOGLE_ANALYTICS_PRIVATE_KEY: z.string(),
+  GOOGLE_ANALYTICS_PROPERTY_ID: z.string(),
+  GOOGLE_CLIENT_EMAIL: z.string(),
+  NEXT_PUBLIC_API_BASE_URL: z.string(),
+  NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
+  NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string(),
+  NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().optional(),
+  NEXT_PUBLIC_SENTRY_DSN: z.string(),
+  NEXT_RUNTIME: z.enum(["nodejs", "edge"]),
+  OCTOKIT_API: z.string(),
+  REDDIT_CLIENT_ID: z.string(),
+  REDDIT_CLIENT_SECRET: z.string(),
+  SENTRY_AUTH_TOKEN: z.string().optional(),
+  SENTRY_CI: z
+    .enum(["true", "false"])
+    .transform((v) => v === "true")
+    .optional()
+    .default(false),
+  SENTRY_LOGGER: z
+    .enum(["true", "false"])
+    .transform((v) => v === "true")
+    .optional()
+    .default(false),
+  SENTRY_ORG: z.string().optional(),
+  SENTRY_PROJECT: z.string().optional(),
+  YOUTUBE_API_KEY: z.string(),
+  PODCAST_INDEX_AUTH_KEY: z.string().optional(),
+  PODCAST_INDEX_SECRET_KEY: z.string().optional(),
+});
+
+export type EnvType = z.infer<typeof envVariables>;
+
+envVariables.parse(process.env);
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,44 +1,14 @@
-import { z } from "zod";
-
-const envVariables = z.object({
-  CLOUDFLARE_ACCOUNT_ID: z.string(),
-  CLOUDFLARE_API_TOKEN: z.string(),
-  CLOUDFLARE_KV_NAMESPACE_ID: z.string(),
-  ENABLE_CATALOG_UPDATE: z.boolean(),
-  FIREBASE_CLIENT_EMAIL: z.string(),
-  FIREBASE_PRIVATE_KEY: z.string(),
-  GOOGLE_ANALYTICS_PRIVATE_KEY: z.string(),
-  GOOGLE_ANALYTICS_PROPERTY_ID: z.string(),
-  GOOGLE_CLIENT_EMAIL: z.string(),
-  NEXT_PUBLIC_API_BASE_URL: z.string(),
-  NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
-  NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string(),
-  NEXT_PUBLIC_SENTRY_DSN: z.string(),
-  OCTOKIT_API: z.string(),
-  REDDIT_CLIENT_ID: z.string(),
-  REDDIT_CLIENT_SECRET: z.string(),
-  SENTRY_AUTH_TOKEN: z.string(),
-  SENTRY_CI: z.boolean(),
-  SENTRY_LOGGER: z.boolean(),
-  SENTRY_ORG: z.string(),
-  SENTRY_PROJECT: z.string(),
-  YOUTUBE_API_KEY: z.string(),
-  PODCAST_INDEX_BASE_URL: z.string(),
-  PODCAST_INDEX_AUTH_KEY: z.string(),
-  PODCAST_INDEX_SECRET_KEY: z.string(),
-});
-
-envVariables.parse(process.env);
+import type { EnvType } from "./env";
 
 declare module "lite-youtube-embed" {
-  declare class LiteYTEmbed extends HTMLElement {
+  class LiteYTEmbed extends HTMLElement {
     async getYTPlayer(): Promise<YT.Player>;
   }
 }
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends z.infer<typeof envVariables> {}
+    interface ProcessEnv extends EnvType {}
   }
   interface HTMLElementTagNameMap {
     "lite-youtube": LiteYTEmbed;
